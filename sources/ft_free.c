6e2f2ed47f053a7fd6ec8fc98e5fb0d816d8cf2b
Sergio Balcorta <sergio.shearing@gmail.com>
2018-07-18 00:00:23 -0700

    started with ft_free but need to restructure

--- sources/ft_free.c
+++ sources/ft_free.c
@@ -20,7 +20,7 @@ void	*evaluate_zone(t_head *header, void *ptr)
 	while (chunk != NULL)
 	{
 		if (chunk->chunk_start == ptr && (chunk->used == 1))
-			return (ptr);
+			return (chunk);
 		chunk = chunk->next_chunk;
 	}
 	return (NULL);
@@ -47,15 +47,70 @@ void	*find_ptr(void *ptr, int *index)
 	return (NULL);
 }
 
+t_head	*empty_zone(t_head *head)
+{
+	t_chunk	*chunk;
+
+	while (head != NULL)
+	{
+		chunk = head->meta_start;
+		while (chunk != NULL)
+		{
+			if (chunk->used == 1)
+				break ;
+			if (chunk->next_chunk == NULL)
+				break ;
+			chunk = chunk->next_chunk;
+		}
+		if (chunk != NULL && chunk->next_chunk != NULL && chunk->used == 0)
+			return (head);
+		head = head->next_zone;
+	}
+	return (NULL);
+}
+
+void	deallocate_zone(int index, t_head *head)
+{
+	t_head	*next;
+	t_head	*prev;
+
+	prev = g_slabs[index];
+	if (prev != head)
+	{
+		while (prev->next_zone != head)
+			prev = prev->next_zone;
+		next = head->next_zone;
+		prev->next_zone = next;
+	}
+	else
+		g_slabs[index] = NULL;
+	munmap(head, head->bytes_per_zone);
+}
+
+/*
+*	change below so.....
+*	first the zone is found and address returned
+	then, the chunk pointer is found and returned
+	set chunk->used to 0
+	then check the zone if empty
+		if empty, munmap the zone
+*/
+
+
 void	ft_free(void *ptr)
 {
-	void	*result;
 	int		index;
+	t_chunk	*result;
+	int		before;
+	t_head	*head;
 
 	index = 0;
 	if (ptr == NULL)
 		return ;
 	result = find_ptr(ptr, &index);
-	//dealocate(result);
-	printf("\t\t\t\tINDEX: %d\tresult: %p\n", index, result);
+	before = result->used;
+	result->used = 0;
+	if (result != NULL && (head = empty_zone()) != NULL)
+		deallocate_zone(index, head);
+	//printf("\t\t\t\tINDEX: %d\tresult: %p\tbefore: %d\tafter: %d\n", index, &result, before, result->used);
 }
